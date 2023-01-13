61e07ad59fea0238e924e30108038b1183117a25
Kiartan <krzysztofbroda@empyrean.games>
2023-01-13 15:36:57 +0100

    Constructor corrected. Now the code is working

--- fixture/application.py
+++ fixture/application.py
@@ -4,7 +4,7 @@ from fixture.session import SessionHelper
 
 
 class Application:
-    def __int__(self):
+    def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
