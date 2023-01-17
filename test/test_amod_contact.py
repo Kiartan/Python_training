017d2e5ae78936b078b257d00e19aab0db8d9dd1
Kiartan <krzysztofbroda@empyrean.games>
2023-01-17 10:38:30 +0100

    Added test for modifying the first contact and group on a list

---
+++ test/test_amod_contact.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+def test_modify_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact()
+    app.session.logout()
---
+++ test/test_amod_group.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+def test_modify_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group()
+    app.session.logout()
