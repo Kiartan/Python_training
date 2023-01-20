df2dc93db9da84876cc4a416c237a9d6e8121fe5
Kiartan <krzysztofbroda@empyrean.games>
2023-01-20 11:39:24 +0100

    Added fixture validation before each test. Login and logout are now within the fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,20 +3,16 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="test", middlename="test test", lastname="ccc", nickname="ddd",
                                title="eee", company="fff", address="ggg", phone_home="hhh", phone_mobile="iii",
                                phone_work="jjj", fax="kkk", email="lll", email2="mmm", email3="nnn",
                                homepage="ooo", bday="10", bmonth="January", byear="2000", aday="5",
                                amonth="February", ayear="2010", address2="ppp", phone2="qqq", notes="rrr"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                                title="", company="", address="", phone_home="", phone_mobile="",
                                phone_work="", fax="", email="", email2="", email3="",
                                homepage="", bday="-", bmonth="-", byear="", aday="-",
                                amonth="-", ayear="", address2="", phone2="", notes=""))
-    app.session.logout()
--- test/test_amod_contact.py
+++ test/test_amod_contact.py
@@ -3,11 +3,9 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="edit1", middlename="edit1", lastname="edit1", nickname="edit1",
                                              title="edit1", company="edit1", address="edit1", phone_home="edit2",
                                              phone_mobile="edit2", phone_work="edit2", fax="edit2", email="edit2",
                                              email2="edit2", email3="edit2", homepage="edit2", bday="18", bmonth="May",
                                              byear="edit3", aday="13", amonth="June", ayear="edit3", address2="edit4",
                                              phone2="edit4", notes="edit4"))
-    app.session.logout()
