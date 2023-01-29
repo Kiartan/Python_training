d64f60ca0be87142e8a61052f753bb801ece61a1
Kiartan <Krzybroda@wp.pl>
2023-01-29 18:45:15 +0100

    Added list operations for tests of groups

--- geometry.py
+++ geometry.py
@@ -1,9 +1,10 @@
 
 from geom2d.point import Point
 
-l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
-# l2 = [Point(0, 0), Point(1, 2), Point(2, 1)]
-l2 = list(l1)
-l2[0] = Point(0, 0)
+l = list(map(lambda i: Point(i, i*i), range(-5, 6)))
 
-print(l1 == l2)
+l2 = list(filter(lambda p: p.x % 2 == 0, l))
+
+
+print(l)
+print(l2)
