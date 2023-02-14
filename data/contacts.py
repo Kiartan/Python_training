6fee607d6d9795f66d201f3f52ab9083505c1307
Kiartan <Krzybroda@wp.pl>
2023-02-14 10:02:51 +0100

    Added test data generators for groups and contacts

---
+++ data/contacts.py
@@ -0,0 +1,18 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="first1", middlename="middle1", lastname="last1", nickname="nick1", title="title1",
+            company="company1", address="address11", phone_home="phhome1", phone_mobile="phmob1", phone_work="phwork1",
+            fax="fax1", email="email11", email2="email21", email3="email31", homepage="homepg1", bday="1",
+            bmonth="February", byear="1111", aday="5", amonth="September", ayear="1234", address2="address21",
+            phone2="phone21", notes="notes1"),
+    Contact(firstname="first2", middlename="middle2", lastname="last2", nickname="nick2", title="title2",
+            company="company2", address="address12", phone_home="phhome2", phone_mobile="phmob2", phone_work="phwork2",
+            fax="fax2", email="email12", email2="email22", email3="email32", homepage="homepg2", bday="10",
+            bmonth="May", byear="2222", aday="15", amonth="November", ayear="2345", address2="address22",
+            phone2="phone22", notes="notes2")
+]
+
+
+
