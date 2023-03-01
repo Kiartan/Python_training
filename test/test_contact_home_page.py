1738dd01b85bd86c4bf8ae7445b744dc181e81d6
Kiartan <Krzybroda@wp.pl>
2023-03-01 15:17:31 +0100

    test_contact_on_home_page update. Now itc compares lists of contacts from home page and database

--- test/test_contact_home_page.py
+++ test/test_contact_home_page.py
@@ -1,37 +1,35 @@
-import re
-# from model.group import Group
-# from random import randrange
+# import re
+from model.group import Group
 
 # test comparing all fields shown in home page with information from database for all contacts
 
 
 def test_contact_on_home_page(app, db):
-    # contact_list = app.contact.get_contact_list()
-    # index = randrange(len(contact_list))
-    contacts_from_home_page = app.contact.get_contact_list()
-    contacts_from_db = db.get_contact_list()
-    # contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contacts_from_home_page.lastname == contacts_from_db.lastname  # lastname
-    assert contacts_from_home_page.firstname == contacts_from_db.firstname  # firstname
-    assert contacts_from_home_page.address == contacts_from_db.address  # address
-    # emails
-    assert contacts_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db)
-    # phones
-    assert contacts_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db)
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Group.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Group.id_or_max)
+    assert contacts_from_home_page == contacts_from_db
+    # for x in contacts_from_home_page and y in contacts_from_db:
+        # assert x.lastname == y.lastname  # lastname
+        # assert x.firstname == y.firstname  # firstname
+        # assert x.address == y.address  # address
+        # emails
+        # assert x.all_emails_from_home_page == merge_emails_like_on_home_page(y)
+        # phones
+        # assert x.all_phones_from_home_page == merge_phones_like_on_home_page(y)
 
 
-def clear(s):
-    return re.sub("[() -]", "", s)
+# def clear(s):
+    # return re.sub("[() -]", "", s)
 
 
-def merge_phones_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.phone_home, contact.phone_mobile,
-                                        contact.phone_work, contact.phone2]))))
+# def merge_phones_like_on_home_page(contact):
+    # return "\n".join(filter(lambda x: x != "",
+                            # map(lambda x: clear(x),
+                                # filter(lambda x: x is not None,
+                                       # [contact.phone_home, contact.phone_mobile,
+                                        # contact.phone_work, contact.phone2]))))
 
 
-def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3])))
+# def merge_emails_like_on_home_page(contact):
+    # return "\n".join(filter(lambda x: x != "",
+                            # filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3])))
